214789a41e6c64f2cb43aaa5f04a8d23cc3254ab
AlanKeeps <alan.keeps.walking@gmail.com>
2022-09-08 17:40:47 +0300

    добавил диалог для включения gps

---
+++ app/src/main/java/com/example/weatherapi/DialogManager.kt
@@ -0,0 +1,25 @@
+package com.example.weatherapi
+
+import android.app.AlertDialog
+import android.content.Context
+
+object DialogManager {
+    fun locationSettingsDialog(context: Context, listener: Listener){
+        val builder = AlertDialog.Builder(context)
+        val dialog = builder.create()
+        dialog.setTitle("Enable location?")
+        dialog.setMessage("Location disabled. Do you want enable location?")
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Ok"){_,_ ->
+            listener.onClick()
+            dialog.dismiss()
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel"){_,_ ->
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    interface Listener{
+        fun onClick()
+    }
+}
